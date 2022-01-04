6d9375793741c3c066973682bf7cc2a3aadecac8
Matthew Castrigno <matthewcastrigno@u.boisestate.edu>
2022-01-04 15:18:58 -0700

    inital build

---
+++ xstate/src/MattsMachine.jsx
@@ -0,0 +1,39 @@
+import { createMachine } from "xstate";
+import { useMachine } from "@xstate/react";
+
+const promiseMachine = createMachine({
+  id: "promise",
+  initial: "pending",
+  states: {
+    pending: {
+      on: {
+        RESOLVE: { target: "resolved" },
+        REJECT: { target: "rejected" },
+      },
+    },
+    resolved: {
+      type: "final",
+    },
+    rejected: {
+      type: "final",
+    },
+  },
+});
+const MattsMachine = () => {
+  const [state, send] = useMachine(promiseMachine);
+
+  return (
+    <div>
+      {/** You can listen to what state the service is in */}
+      {state.matches("pending") && <p>Loading...</p>}
+      {state.matches("rejected") && <p>Promise Rejected</p>}
+      {state.matches("resolved") && <p>Promise Resolved</p>}
+      <div>
+        {/** You can send events to the running service */}
+        <button onClick={() => send("RESOLVE")}>Resolve</button>
+        <button onClick={() => send("REJECT")}>Reject</button>
+      </div>
+    </div>
+  );
+};
+export default MattsMachine;
